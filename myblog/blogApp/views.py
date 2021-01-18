996af5dd1a83d85497d48ad40cb356e78194f782
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-01-18 20:43:01 +0100

    Extendet posts model and changed detail layout

--- myblog/blogApp/views.py
+++ myblog/blogApp/views.py
@@ -59,7 +59,7 @@ def Contact(request):
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 4
+    paginate_by = 3
 
 class PostDetail(generic.DetailView):
     model = Post
